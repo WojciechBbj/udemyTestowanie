163870674d3e4ae651119fb02bec56ff69a8ce7a
Wojtek_linus <wojtekbabij@wp.pl>
2021-02-10 22:17:08 +0100

    podzial testu na give, when, then

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -7,15 +7,20 @@ public class AccountTest {
 
     @Test
     public void myTest() {
+        //given
+        //when
         Account newAccount = new Account();
+        //then
         assertFalse(newAccount.isActive(), "Check if new account is not active");
     }
 
     @Test
     void myTest2()  {
+        //given
         Account newAccount = new Account();
-        assertFalse(newAccount.isActive());
+        //when
         newAccount.activate();
+        //then
         assertTrue(newAccount.isActive());
     }
 }
